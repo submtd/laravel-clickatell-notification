6a59dcba059118d1e03dbc02febe56b306b06696
Steve Harmeyer <harmeyersteve@gmail.com>
2019-04-02 09:24:45 -0700

    updates

---
+++ src/Providers/LaravelClickatellNotificationServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Submtd\LaravelClickatellNotification;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelClickatellNotificationServiceProvider extends ServiceProvider
+{
+}
